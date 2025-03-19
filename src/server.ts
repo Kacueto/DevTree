8eeafb710094bb0d025f9c621c414281f9664c81
kacueto <kacueto@uninorte.edu.co>
2025-03-18 23:31:39 -0500

    Seccion10 Agregue los CORS al backend

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,14 @@
 import express from 'express'
 import 'dotenv/config'
 import router from './router'
-
+import cors from 'cors'
 import { connectDB } from './config/db'
-
+import { corsConfig } from './config/cors'
+connectDB()
 const app = express()
 
-connectDB()
+//cors
+app.use(cors(corsConfig))
 // Leer datos de formularios
 app.use(express.json())
 
